7c70e16d0a91998019ab09e70968a7fab0a73495
Barry Quek JJ <barry.quek@generalassemb.ly>
2023-04-12 09:43:24 +0800

    Fixing property read in create procedure to account for correction visa vi clerk's getAuth routine

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -55,7 +55,7 @@ export const postsRouter = createTRPCRouter({
   create: privateProcedure
     .input(z.object({ content: z.string().emoji().min(1).max(255) }))
     .mutation(async ({ ctx, input }) => {
-      const authorId = ctx.currentUser.id;
+      const authorId = ctx.userId;
 
       const post = await ctx.prisma.post.create({
         data: {
