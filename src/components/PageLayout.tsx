ffad3646fca99149309539552f35519d3d95deaf
Barry Quek JJ <barry.quek@generalassemb.ly>
2023-04-22 17:23:43 +0800

    logged attempt at home button

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,6 +1,7 @@
 import { type PropsWithChildren } from "react";
 
 export const PageLayout = (props: PropsWithChildren) => {
+  // NOTE : attempted to put back button here for Post and Profile views with useRouter from next/js, but that won't achieve the desired functionality-- should instead put a 'Home' button
   return (
     <main className="flex h-screen justify-center">
       <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
